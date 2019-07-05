e3633cd67426e66d2418cf52e5023e22a2c8f309
sooo <mail@sooo.space>
2019-07-05 14:26:12 +0900

    feat(pwsh): Enter/Exit ExchangeOnline function

--- home/.config/powershell/Microsoft.PowerShell_profile.ps1
+++ home/.config/powershell/Microsoft.PowerShell_profile.ps1
@@ -6,3 +6,14 @@ function Set-MyPSReadLineColor(){
 }
 
 Set-MyPSReadLineColor
+
+function Enter-ExchangeOnline() {
+    $global:Cred = Get-Credential
+    $global:Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $global:Cred -Authentication Basic -AllowRedirection
+    Import-PSSession $global:Session -DisableNameChecking
+}
+
+function Exit-ExchangeOnline() {
+  Remove-PSSession $global:Session
+  exit
+}
