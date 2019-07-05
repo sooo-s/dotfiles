5acb98635120aa278cc0dde974e3a449f611755d
sooo <mail@sooo.space>
2019-07-05 15:25:38 +0900

    feat(pwsh): export utf8 csv file function

--- home/.config/powershell/Microsoft.PowerShell_profile.ps1
+++ home/.config/powershell/Microsoft.PowerShell_profile.ps1
@@ -7,6 +7,10 @@ function Set-MyPSReadLineColor(){
 
 Set-MyPSReadLineColor
 
+function Export-UTF8-Csv($name='./csv.csv'){
+  $input | Export-Csv -encoding UTF8 $name
+}
+
 function Enter-ExchangeOnline() {
     $global:Cred = Get-Credential
     $global:Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $global:Cred -Authentication Basic -AllowRedirection
